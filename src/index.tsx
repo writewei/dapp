1d051f80fbc46802308cd1a384003676636acd10
Chance Hudson <jchancehud@gmail.com>
2019-01-15 03:43:15 -0600

    Add home component

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,25 @@
-console.log('Hello world');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './components/Home';
+import { Provider } from 'mobx-react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+const stores = {};
+
+Object.assign(document.body.style, {
+  margin: 'auto',
+  'background-color': '#222',
+  'max-width': '900px',
+  'font-family': 'Helvetica',
+});
+
+ReactDOM.render(
+  <Provider { ...stores }>
+    <Router>
+      <>
+        <Route path="/" component={Home} />
+      </>
+    </Router>
+  </Provider>,
+  document.getElementById('app')
+);
