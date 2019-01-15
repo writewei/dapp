a3cdd8889f8e7a6efb7ac855146dc4e03dc08486
Chance Hudson <jchancehud@gmail.com>
2019-01-15 06:25:54 -0600

    Use cidbadge for generating a badge of the document

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import MDContainer from './MDContainer';
 import styled from 'styled-components';
 import { inject } from 'mobx-react';
+import cidbadge from 'cidbadge';
 
 const TextInput = styled.textarea`
   width: 100%;
@@ -16,18 +17,29 @@ export default class Home extends React.Component<{
   state = {
     content: `# Hi There
 
-Write something to get started.`
+Write something to get started.`,
+    cid: 'QmWiNA...8GsHf9'
   };
 
   contentChanged = (event: any) => {
+    const content = event.target.value;
+    // Add the content to the local ipfs node on every change
+    // This can't stay in lol
+    this.props.node.files.add(Buffer.from(content, 'utf8'), (err: any, data: any) => {
+      if (err || !data.length) return;
+      this.setState({
+        cid: data[0].path
+      });
+    });
     this.setState({
-      content: event.target.value
+      content
     });
   };
 
   render() {
     return (
       <>
+        <span dangerouslySetInnerHTML={{ __html: cidbadge(this.state.cid)}} />
         <TextInput onChange={this.contentChanged} value={this.state.content} />
         <MDContainer content={this.state.content} />
       </>
