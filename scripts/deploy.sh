9c77a5197b9893182d3a15401b7ececf16e6e50c
Chance Hudson <jchancehud@gmail.com>
2019-01-17 22:21:47 -0600

    Add ci

---
+++ scripts/deploy.sh
@@ -0,0 +1,36 @@
+#/bin/sh
+
+set -e
+
+# Intended to be run in a CI environment
+if [ -z "$CI" ];
+then
+  echo 'Non-ci environment detected, exiting'
+  exit 1
+fi
+
+# Build the web app in ./static
+npm run build
+
+# Install jsipfs
+npm i -g ipfs
+
+# Start a local IPFS node
+jsipfs init
+jsipfs daemon &
+sleep 10
+
+# Load the old CID based on the current dnslinked value
+OLD_CID=$(jsipfs dns writewei.io)
+
+# Load the new CID by adding it to the local IPFS node
+NEW_CID=$(jsipfs add -Qr ./static)
+
+# Unpin the old version
+npx cidhook cidhookd.writewei.io $OLD_CID unpin
+
+# Pin the new version
+npx cidhook cidhookd.writewei.io $NEW_CID pin
+
+# Update the DNS record
+npx dnslink update writewei.io $NEW_CID
