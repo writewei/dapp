b1ad9dab12bd4308a29179dd56b05a881fa823bd
Chance Hudson <jchancehud@gmail.com>
2019-01-17 19:24:32 -0600

    Update CID calculation to only hash

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -10,30 +10,58 @@ const TextInput = styled.textarea`
   resize: vertical;
 `;
 
+const DEFAULT_TEXT = `# Hi There
+
+I bet you're thinking, what is this thing.
+
+### This is a subsection
+
+- this is a bullet point
+- another level in
+
+
+\`\`\`
+() => console.log('hello world');
+\`\`\`
+
+![](https://media.mnn.com/assets/images/2018/07/cat_eating_fancy_ice_cream.jpg.838x0_q80.jpg)
+
+A bit smaller
+
+<img src="https://media.mnn.com/assets/images/2018/07/cat_eating_fancy_ice_cream.jpg.838x0_q80.jpg" width=300 />
+`;
+
 @inject('node')
 export default class Home extends React.Component<{
   node: any
 }> {
   state = {
-    content: `# Hi There
-
-Write something to get started.`,
-    cid: 'QmWiNA...8GsHf9'
+    content: DEFAULT_TEXT,
+    cid: ''
   };
+
+  componentDidMount() {
+    this.calculateCid();
+  }
+
+  calculateCid = () => {
+    this.props.node.files.add(
+      Buffer.from(this.state.content, 'utf8'),
+      {
+        onlyHash: true
+      },
+      (err: any, data: any) => {
+        if (err || !data.length) return;
+        this.setState({
+          cid: data[0].path
+        });
+      }
+    );
+  }
 
   contentChanged = (event: any) => {
     const content = event.target.value;
-    // Add the content to the local ipfs node on every change
-    // This can't stay in lol
-    this.props.node.files.add(Buffer.from(content, 'utf8'), (err: any, data: any) => {
-      if (err || !data.length) return;
-      this.setState({
-        cid: data[0].path
-      });
-    });
-    this.setState({
-      content
-    });
+    this.setState({ content }, this.calculateCid);
   };
 
   render() {
