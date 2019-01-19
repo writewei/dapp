0e82be499b266f22dea15a936dae46a3f4747fc6
Chance Hudson <jchancehud@gmail.com>
2019-01-19 02:07:23 -0600

    Make document pinning functional

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import MDContainer from './MDContainer';
 import styled from 'styled-components';
 import { inject } from 'mobx-react';
-import ButtonHeader from './ButtonHeader';
+import cidbadge from 'cidbadge';
+import _cidhook from '../stores/cidhook';
 
 const TextInput = styled.textarea`
   width: 100%;
   height: 200px;
   resize: vertical;
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
 `;
 
 const DEFAULT_TEXT = `# Hi There
@@ -31,9 +37,10 @@ A bit smaller
 <img src="https://media.mnn.com/assets/images/2018/07/cat_eating_fancy_ice_cream.jpg.838x0_q80.jpg" width=300 />
 `;
 
-@inject('node')
+@inject('node', 'cidhook')
 export default class Home extends React.Component<{
-  node: any
+  node: any,
+  cidhook?: _cidhook
 }> {
   state = {
     content: DEFAULT_TEXT,
@@ -57,17 +64,43 @@ export default class Home extends React.Component<{
         });
       }
     );
-  }
+  };
 
   contentChanged = (event: any) => {
     const content = event.target.value;
     this.setState({ content }, this.calculateCid);
   };
 
+  pinContent = (dryRun: boolean = true) => {
+    this.props.node.files.add(
+      Buffer.from(this.state.content, 'utf8'),
+      {
+        onlyHash: dryRun
+      },
+      (err: any, data: any) => {
+        if (err) {
+          return alert(`Error: ${err}`);
+        } else if (!data.length) {
+          return alert(`Error: 0 length data received.`);
+        }
+        if (!dryRun) {
+          console.log(`pinned ipfs cid: ${data[0].path}`);
+        }
+        const cid = data[0].path;
+        this.setState({ cid });
+        this.props.cidhook.pin(cid);
+      }
+    );
+  };
+
   render() {
     return (
       <>
-        <ButtonHeader cid={this.state.cid} />
+        <Container>
+          <span dangerouslySetInnerHTML={{ __html: cidbadge(this.state.cid)}} />
+          <a href={`https://ipfs.io/ipfs/${this.state.cid}`}>{this.state.cid}</a>
+          <button onClick={() => this.pinContent(false)}>pin</button>
+        </Container>
         <TextInput onChange={this.contentChanged} value={this.state.content} />
         <MDContainer content={this.state.content} />
       </>
