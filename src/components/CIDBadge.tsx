3e36025217d01f4418fd0e61a8da6781fc74d97b
Chance Hudson <jchancehud@gmail.com>
2019-01-19 23:09:23 -0600

    Use react router redirect on cidbadge

--- src/components/CIDBadge.tsx
+++ src/components/CIDBadge.tsx
@@ -1,14 +1,35 @@
 import React from 'react';
 import cidbadge from 'cidbadge';
+import { Redirect } from 'react-router-dom';
+import styled from 'styled-components';
+
+const ClickableDiv = styled.div`
+  cursor: pointer;
+`;
 
 export default class CIDBadgeContainer extends React.Component<{
   cid: string
 }> {
+  state = {
+    isRedirecting: false,
+    redirectPath: ''
+  };
+  componentDidUpdate() {
+    if (this.state.isRedirecting) this.setState({ isRedirecting: false });
+  }
   render() {
+    if (this.state.isRedirecting) {
+      return <Redirect to={this.state.redirectPath} />;
+    }
     return (
-      <a href={`/edit/${this.props.cid}`}>
+      <ClickableDiv onClick={() => {
+        this.setState({
+          isRedirecting: true,
+          redirectPath: `/edit/${this.props.cid}`
+        });
+      }}>
         <span dangerouslySetInnerHTML={{ __html: cidbadge(this.props.cid)}} />
-      </a>
+      </ClickableDiv>
     );
   }
 }
