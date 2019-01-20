639212c2a79b017a26551fc08c6ef778021e5792
Chance Hudson <jchancehud@gmail.com>
2019-01-19 20:28:36 -0600

    Add button to go to edit screen

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,20 +3,24 @@ import { inject, observer } from 'mobx-react';
 import DocumentStore, { Document } from '../stores/DocumentStore';
 import CIDBadge from './CIDBadge';
 import WeiDisplay from './WeiDisplay';
-import Edit from './Edit';
 import Cell from './Cell';
+import { Redirect } from 'react-router-dom';
 
 @inject('ethereum', 'documentStore')
 @observer
 export default class Home extends React.Component<{
   documentStore?: DocumentStore
 }> {
-  state = {};
+  state = {
+    toEdit: false
+  };
   render() {
+    if (this.state.toEdit) {
+      return <Redirect to='/edit' />
+    }
     return (
       <>
         {this.props.documentStore.documents.map((document: Document) => {
-          console.log('test', document);
           return (
             <Cell key={document.cid}>
               <CIDBadge cid={document.cid} />
@@ -24,7 +28,9 @@ export default class Home extends React.Component<{
             </Cell>
           );
         })}
-        <Edit />
+        <button onClick={() => {
+          this.setState({ toEdit: true });
+        }}>Create a Document</button>
       </>
     );
   }
