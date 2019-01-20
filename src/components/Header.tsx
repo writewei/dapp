b4275cd618fdce99d2db5ea116aaffdaca6ad389
Chance Hudson <jchancehud@gmail.com>
2019-01-19 20:36:37 -0600

    Header home redirect logic

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,7 @@ import Cell from './Cell';
 import styled from 'styled-components';
 import DocumentStore from '../stores/DocumentStore';
 import EthereumStore from '../stores/EthereumStore';
+import { Redirect } from 'react-router-dom';
 
 const Title = styled.div`
   font-size: 32px;
@@ -25,11 +26,22 @@ export default class Header extends React.Component<{
   documentStore?: DocumentStore,
   ethereum?: EthereumStore
 }> {
+  state = {
+    toHome: false
+  };
+  componentDidUpdate() {
+    if (this.state.toHome) this.setState({ toHome: false });
+  }
   render() {
+    if (this.state.toHome) {
+      return <Redirect to='/' />;
+    }
     return (
       <Cell>
         <HFlex>
-          <div>
+          <div onClick={() => {
+            this.setState({ toHome: true });
+          }}>
             <Title>writewei</Title>
             <span>A decentralized writing platform</span>
           </div>
