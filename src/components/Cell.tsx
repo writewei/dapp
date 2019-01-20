58f67fbbc1e29bd97242e65ad19719e41e253b55
Chance Hudson <jchancehud@gmail.com>
2019-01-20 02:27:16 -0600

    Adjust cell styling

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export default styled.div`
-  box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 8px;
+  box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 4px;
   padding: 8px;
   margin-top: 8px;
 `;
