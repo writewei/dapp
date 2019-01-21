e07c14fe55fbefe0e1da6003c5407c46d6a88812
Chance Hudson <jchancehud@gmail.com>
2019-01-20 18:15:14 -0600

    Rearrange document cell layout

--- src/components/DocumentCell.tsx
+++ src/components/DocumentCell.tsx
@@ -3,6 +3,18 @@ import Cell from './Cell';
 import CIDBadge from './CIDBadge';
 import WeiDisplay from './WeiDisplay';
 import { Document } from '../stores/DocumentStore';
+import styled from 'styled-components';
+
+const HFlex = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const VFlex = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
 
 export default class DocumentCell extends React.Component<{
   document: Document
@@ -10,8 +22,15 @@ export default class DocumentCell extends React.Component<{
   render() {
     return (
       <Cell>
-        <CIDBadge cid={this.props.document.cid} />
-        <WeiDisplay wei={this.props.document.weiValue} />
+        <HFlex>
+          <VFlex>
+            <CIDBadge cid={this.props.document.cid} />
+          </VFlex>
+          <VFlex>
+            <WeiDisplay wei={this.props.document.weiValue} />
+            <button onClick={() => {}}>Pay Author</button>
+          </VFlex>
+        </HFlex>
       </Cell>
     );
   }
