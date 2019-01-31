63d6b534dc686e0aca121df9af26ed1daf3fbfa1
Chance Hudson <jchancehud@gmail.com>
2019-01-31 03:28:16 -0600

    Add star signal test code

--- src/stores/IPFSStore.ts
+++ src/stores/IPFSStore.ts
@@ -7,10 +7,18 @@ export default class _IPFS {
   node: any;
 
   constructor() {
-    this.node = new IPFS();
+    this.node = new IPFS({
+      config: {
+        Addresses: {
+          Swarm: [
+            // '/dns4/starsignal.writewei.io/tcp/443/p2p-webrtc-star'
+          ]
+        }
+      }
+    });
     this.node.on('ready', () => {
       this.isReady = true;
-      this.node.bootstrap.add('/dns4/ipfs.writewei.io/tcp/443/ipfs/QmcLccRcV915rV5cbfxzF263rNe45BSXfaPFQjGqn8LXAQ');
+      // this.node.bootstrap.add('/dns4/ipfs.writewei.io/tcp/443/ipfs/QmcLccRcV915rV5cbfxzF263rNe45BSXfaPFQjGqn8LXAQ');
     });
     this.node.on('error', (err: any) => {
       console.log('IPFS node error:', err);
